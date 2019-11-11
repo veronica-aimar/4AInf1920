ec6df0379a493eb0919a0435f765c6097564e8a4
veronica-aimar <57140786+veronica-aimar@users.noreply.github.com>
2019-11-11 10:23:50 +0100

    Update Contenitore.java

--- 19Bottiglie/src/it/itis/cuneo/Contenitore.java
+++ 19Bottiglie/src/it/itis/cuneo/Contenitore.java
@@ -1,4 +1,3 @@
-package it.itis.cuneo;
 import java.util.Arrays;
 import javax.naming.ContextNotEmptyException;
 import java.lang.Throwable;
@@ -7,9 +6,10 @@ import java.lang.Throwable;
  * Created by inf.aimarv1906 on 31/10/2019.
  */
 public class Contenitore {
-    public static final int MAX_BOTTIGLIE = 6;
+    public static final int MAX_BOTTIGLIE = InputOutputUtility.leggiNumero("Inserisci il numero massimo di bottiglie per contenitore: ");
     private Bottiglia[] vBottiglie;
     private int cntBottiglie;
+    private int length;
 
     //COSTRUTTORI
 
@@ -17,6 +17,7 @@ public class Contenitore {
         this.vBottiglie = new Bottiglia[MAX_BOTTIGLIE];
         cntBottiglie = 0;
     }
+
     public void addBottiglia(Bottiglia b) throws ContenitorePienoException {
         if(cntBottiglie+1 >= MAX_BOTTIGLIE)
         {
@@ -24,5 +25,57 @@ public class Contenitore {
         }
             vBottiglie[cntBottiglie] = b;
             cntBottiglie++;
+    }
+
+    public int setContenitore(Bottiglia bottiglia, int posizione) {
+        if(posizione < 0 || posizione > MAX_BOTTIGLIE)
+        {
+            return -1;
+        }
+        if(vBottiglie[posizione] != null)
+        {
+            return -2;
+        }
+        vBottiglie[posizione] = bottiglia;
+        return 1;
+    }
+    public Bottiglia getContenitore(int posizione) {
+        if(vBottiglie[posizione] == null || posizione < 0 || posizione > MAX_BOTTIGLIE)
+        {
+            return null;
+        }
+        return vBottiglie[posizione];
+    }
+
+    public int killProgramma(int posizione) {
+        if(posizione < 0 || posizione > MAX_BOTTIGLIE)
+        {
+            return -1;
+        }
+        if(vBottiglie[posizione] == null)
+        {
+            return -2;
+        }
+        vBottiglie[posizione] = null;
+        return 1;
+    }
+
+    public int getN() {
+        int i, contatore = 0;
+        for(i=0; i<this.length; i++) {
+        if(vBottiglie[i] != null)
+            {
+                contatore++;
+            }
+        }
+        return contatore;
+    }
+
+    public Bottiglia cercaBottiglia(int posizione) {
+        if(posizione < 0 || posizione > MAX_BOTTIGLIE || vBottiglie[posizione] == null)
+        {
+            return null;
+        }
+        return vBottiglie[posizione];
     }
 }
